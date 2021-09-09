2c65a943ac553d341f223150bc8f38cb8273c7c5
Jared Donovan <jdono100@gmail.com>
2021-09-08 22:17:32 -0500

    RESTful API routes removed

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
-// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -32,8 +31,6 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
-// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
